f63786e2b96d22f305218eddaf44f35fd9029e3e
ramy.ramzy <ramyahramzy@hotmail.com>
2022-07-25 18:40:20 +0300

    Remove GraphQL

--- src/main/java/com/ramy/recipes/repository/IngredientRepo.java
+++ src/main/java/com/ramy/recipes/repository/IngredientRepo.java
@@ -1,7 +1,12 @@
 package com.ramy.recipes.repository;
 
 import com.ramy.recipes.model.Ingredient;
+import com.ramy.recipes.model.Recipe;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IngredientRepo extends JpaRepository<Ingredient, Long> {
+    List<Ingredient> findByRecipe(Recipe recipe);
+    List<Ingredient> findByDescriptionContainingIgnoreCase(String description);
 }
--- src/main/java/com/ramy/recipes/repository/RecipeRepo.java
+++ src/main/java/com/ramy/recipes/repository/RecipeRepo.java
@@ -5,7 +5,49 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface RecipeRepo extends JpaRepository<Recipe , Long> {
-    public List<Recipe> getRecipeByServe(Integer serve  );
-    public List<Recipe> getRecipeByVegetarian(boolean vegetarian  );
+public interface RecipeRepo extends JpaRepository<Recipe, Long> {
+
+
+    public List<Recipe> getRecipeByVegetarian(boolean vegetarian);
+    public List<Recipe> getRecipeByServe(int serve);
+    public List<Recipe> getRecipeByIngredients_DescriptionContainingIgnoreCase(String description);
+    public List<Recipe> getRecipeByIngredients_DescriptionNotContainingIgnoreCase(String description);
+    public List<Recipe> getRecipeByInstructionContainingIgnoreCase( String instruction);
+
+
+    public List<Recipe> getRecipeByVegetarianAndServe(boolean vegetarian,int serve);
+    public List<Recipe> getRecipeByVegetarianAndIngredients_DescriptionContainingIgnoreCase(boolean vegetarian,String description);
+    public List<Recipe> getRecipeByVegetarianAndIngredients_DescriptionNotContainingIgnoreCase(boolean vegetarian,String description);
+    public List<Recipe> getRecipeByVegetarianAndInstructionContainingIgnoreCase(boolean vegetarian, String instruction);
+
+
+    public List<Recipe> getRecipeByServeAndIngredients_DescriptionContainingIgnoreCase(int serve,String description);
+    public List<Recipe> getRecipeByServeAndIngredients_DescriptionNotContainingIgnoreCase(int serve,String description);
+    public List<Recipe> getRecipeByServeAndInstructionContainingIgnoreCase(int serve,String instruction);
+
+
+    public List<Recipe> getRecipeByIngredients_DescriptionContainingIgnoreCaseAndInstructionContainingIgnoreCase(String description,String instruction);
+    public List<Recipe> getRecipeByIngredients_DescriptionNotContainingIgnoreCaseAndInstructionContainingIgnoreCase(String description,String instruction);
+
+
+    public List<Recipe> getRecipeByVegetarianAndServeAndIngredients_DescriptionContainingIgnoreCase(boolean vegetarian,int serve,String description);
+    public List<Recipe> getRecipeByVegetarianAndServeAndIngredients_DescriptionNotContainingIgnoreCase(boolean vegetarian,int serve,String description);
+    public List<Recipe> getRecipeByVegetarianAndServeAndInstructionContainingIgnoreCase(boolean vegetarian,int serve,String instruction);
+    public List<Recipe> getRecipeByVegetarianAndIngredients_DescriptionContainingIgnoreCaseAndInstructionContainingIgnoreCase(boolean vegetarian,String description,String instruction);
+    public List<Recipe> getRecipeByVegetarianAndIngredients_DescriptionNotContainingIgnoreCaseAndInstructionContainingIgnoreCase(boolean vegetarian,String description,String instruction);
+
+    public List<Recipe> getRecipeByServeAndIngredients_DescriptionContainingIgnoreCaseAndInstructionContainingIgnoreCase(int serve,String description,String instruction);
+    public List<Recipe> getRecipeByServeAndIngredients_DescriptionNotContainingIgnoreCaseAndInstructionContainingIgnoreCase(int serve,String description,String instruction);
+
+
+    public List<Recipe> getRecipeByVegetarianAndServeAndIngredients_DescriptionContainingIgnoreCaseAndInstructionContainingIgnoreCase(boolean vegetarian,int serve,String description , String instruction);
+    public List<Recipe> getRecipeByVegetarianAndServeAndIngredients_DescriptionNotContainingIgnoreCaseAndInstructionContainingIgnoreCase(boolean vegetarian,int serve,String description , String instruction);
+
+
+    public List<Recipe> getRecipeByVegetarianAndServeAndIngredients_DescriptionNotContainingIgnoreCaseAndIngredients_DescriptionContainingIgnoreCaseAndInstructionContainingIgnoreCase(boolean vegetarian,int serve,String description ,String description1, String instruction);
+
+
+
+
+
 }
